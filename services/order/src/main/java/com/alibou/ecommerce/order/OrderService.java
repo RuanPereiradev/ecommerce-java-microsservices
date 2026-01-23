0877b6848d747e823494cc8f2921e9caa275e035
RuanPereiradev <ruanpereira@alu.ufc.br>
2026-01-23 15:55:43 -0300

    order microservice

---
+++ services/order/src/main/java/com/alibou/ecommerce/order/OrderService.java
@@ -0,0 +1,12 @@
+package com.alibou.ecommerce.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    public Integer createdOrder(OrderRequest request){
+        return null;
+    }
+}
